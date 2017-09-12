be256c040710e313b9aa27cb6f61e682e3646a8a
Zhenya <genya_3@mail.ru>
2017-09-13 00:50:22 +0300

    Initial commit

---
+++ app/src/main/java/by/bstu/ben/basejava/JavaTestTepy.java
@@ -0,0 +1,25 @@
+package by.bstu.ben.basejava;
+
+import android.util.Log;
+
+/**
+ * Created by Администратор on 12.09.2017.
+ */
+
+public class JavaTestTepy {
+
+    static int sint;
+
+    public static void main(String[] args) {
+
+        char symbol = 's';
+        int number = 3;
+        short sNumber = 1;
+        byte bNumber = 101;
+        long lNumber = 4566L;
+        boolean bool = true;
+
+        Log.d("abs","fgh");
+        System.out.println("HELLO!");
+    }
+}
